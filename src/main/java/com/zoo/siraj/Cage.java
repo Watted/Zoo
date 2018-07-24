d802036fb6e06f4708c808ef5a9058b64a28d3a9
Watted <wattk308@gmail.com>
2018-07-24 12:47:56 +0300

    zoo project

---
+++ src/main/java/com/zoo/siraj/Cage.java
@@ -0,0 +1,41 @@
+package com.zoo.siraj;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Cage {
+    private static int idOfCage = 0;
+    private String id;
+    private int size;
+    private List<Animal> contentAnimal;
+
+    public Cage(int size) {
+        this.id = String.valueOf(++idOfCage);
+        this.size = size;
+        this.contentAnimal = new ArrayList<>();
+    }
+
+    public int getSize() {
+        return size;
+    }
+
+    public void setSize(int size) {
+        this.size = size;
+    }
+
+    public List<Animal> getContentAnimal() {
+        return contentAnimal;
+    }
+
+    public void setContentAnimal(List<Animal> contentAnimal) {
+        this.contentAnimal = contentAnimal;
+    }
+
+    public void addAnimal(Animal animal){
+        this.contentAnimal.add(animal);
+    }
+
+    public boolean removeAnimal(Animal animal){
+        return this.contentAnimal.remove(animal);
+    }
+}
---
+++ src/main/java/com/zoo/siraj/Employee.java
@@ -0,0 +1,52 @@
+package com.zoo.siraj;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class Employee {
+    private static int idOfEmployee = 0;
+    private String id;
+    private String name;
+    private List<Animal> treatmentAnimals;
+
+    public Employee(String name) {
+        this.id = String.valueOf(++idOfEmployee);
+        this.name = name;
+        this.treatmentAnimals = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public List<Animal> getTreatmentAnimals() {
+        return treatmentAnimals;
+    }
+
+    public void setTreatmentAnimals(List<Animal> treatmentAnimals) {
+        this.treatmentAnimals = treatmentAnimals;
+    }
+
+    public void addAnimal(Animal animal){
+        this.treatmentAnimals.add(animal);
+    }
+
+    public boolean removeAnimal(Animal animal){
+        return this.treatmentAnimals.remove(animal);
+    }
+
+    public boolean feedAnimals(Animal animal ,Food food, int amount){
+        int index = this.treatmentAnimals.indexOf(animal);
+        if (index !=-1) {
+            Animal animal1 = this.treatmentAnimals.get(index);
+            animal1.updateFoodAmount(amount, food);
+            return true;
+        }
+        return false;
+    }
+}
